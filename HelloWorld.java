885499b4cca7a531d5014c485492609e8124ed4d
B NGUYEN <bnguyen200982@gmail.com>
2023-03-23 21:16:58 +0000

    Added more content to Hello World messge

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World! From Jenkins practice job 1");
+    System.out.println("How is everyone today");
   }
 }
